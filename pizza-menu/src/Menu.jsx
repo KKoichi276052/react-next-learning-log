6e8e42cfb9f560af7f42c9d78ab89be5ebebb212
KK <k.koichi276052@gmail.com>
2024-04-09 10:51:18 +0900

    jonas WIP

---
+++ pizza-menu/src/Menu.jsx
@@ -0,0 +1,12 @@
+import Pizza from "./Pizza";
+
+export default function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
