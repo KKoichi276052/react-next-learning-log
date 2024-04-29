c106946e03c2b737ca173312b4b790ac1ba3a555
KK <k.koichi276052@gmail.com>
2024-04-30 08:01:27 +0900

    jonas state wip

--- jonas-state-app/src/App.jsx
+++ jonas-state-app/src/App.jsx
@@ -9,6 +9,8 @@ const messages = [
 function App() {
   const btnStyle = { backgroundColor: "#7950f2", color: "#fff" };
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
   const nextStep = () => {
     if (step < 3) setStep(step + 1);
   };
@@ -17,26 +19,34 @@ function App() {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button style={btnStyle} onClick={previousStep}>
-          Previous
-        </button>
-        <button style={btnStyle} onClick={nextStep}>
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button style={btnStyle} onClick={previousStep}>
+              Previous
+            </button>
+            <button style={btnStyle} onClick={nextStep}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
