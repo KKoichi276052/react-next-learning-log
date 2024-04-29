39f166f5a92825f5b2b4bf53e8e40e20e12ca6f3
KK <k.koichi276052@gmail.com>
2024-04-29 19:57:33 +0900

    jonas update step

--- jonas-state-app/src/App.jsx
+++ jonas-state-app/src/App.jsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 import "./App.css";
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 function App() {
   const btnStyle = { backgroundColor: "#7950f2", color: "#fff" };
-  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const nextStep = () => {
+    if (step < 3) setStep(step + 1);
+  };
+  const previousStep = () => {
+    if (step > 1) setStep(step - 1);
+  };
 
   return (
-    <>
-      <div className="steps">
-        <div className="numbers">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-        </div>
-
-        <p className="message">Hello</p>
-
-        <div className="buttons">
-          <button style={btnStyle}>Previous</button>
-          <button style={btnStyle}>Next</button>
-        </div>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-    </>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={btnStyle} onClick={previousStep}>
+          Previous
+        </button>
+        <button style={btnStyle} onClick={nextStep}>
+          Next
+        </button>
+      </div>
+    </div>
   );
 }
 
