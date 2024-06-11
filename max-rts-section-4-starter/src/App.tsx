82d51e6211ded732caf3fd0b2ba7e66ea37c23ad
片岡 宏一 <kataokakoichi@Ks-Corporate-M3-Air.local>
2024-06-11 16:41:19 +0900

    max finish section

--- max-rts-section-4-starter/src/App.tsx
+++ max-rts-section-4-starter/src/App.tsx
@@ -1,5 +1,38 @@
+import { useRef } from 'react';
+import Button from './components/Button';
+// import Container from './components/Container';
+import Input from './components/Input';
+import Form, { type FormHandle } from './components/Form';
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  const customForm = useRef<FormHandle>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: number };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
+  return (
+    <main>
+      {/* <Input id="name" label="Your name" type="text" />
+      <Input id="age" label="Your age" type="number" /> */}
+      {/* <Button el="button" href="https://google.com">
+        Button
+      </Button>
+      <Button el="anchor" href="https://google.com">
+        Link
+      </Button> */}
+      {/* <Container as={Button}>Click me</Container> */}
+      {/* <Input label="Test" id="test" ref={input} /> */}
+
+      <Form onSave={handleSave} ref={customForm}>
+        <Input type="text" name="name" label="Name" id="name" />
+        <Input type="number" name="age" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
+    </main>
+  );
 }
 
 export default App;
