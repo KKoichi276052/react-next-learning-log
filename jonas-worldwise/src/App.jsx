a22cbd3eab3b8e50b4257b47a23d70a07449e8af
Koichi Kataoka <k.koichi276052@gmail.com>
2024-07-08 11:13:28 +0900

    jonas world-wise adding city list

--- jonas-worldwise/src/App.jsx
+++ jonas-worldwise/src/App.jsx
@@ -10,8 +10,30 @@ import CountryList from './components/CountryList';
 import Form from './components/Form';
 import './App.css';
 // import('preline');
+import { useState, useEffect } from 'react';
+
+const BASE_URL = 'http://localhost:9000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert(`Error fetching cities. ${error.message}`);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -20,8 +42,18 @@ function App() {
           <Route path="/product" element={<Product />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/app" element={<AppLayout />}>
-            <Route path="cities" element={<CityList />} />
-            <Route path="countries" element={<CountryList />} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="countries"
+              element={<CountryList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="form" element={<Form />} />
           </Route>
           <Route path="/login" element={<Login />} />
--- jonas-worldwise/src/components/CountryList.jsx
+++ jonas-worldwise/src/components/CountryList.jsx
@@ -1,7 +1,23 @@
-export default function CountryList() {
+import styles from './CountryList.module.css';
+import Spinner from './Spinner';
+import CountryItem from './CountryItem';
+import Message from './Message';
+
+export default function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message>Add your first city by clicking on a city on the map</Message>
+    );
+
+  const countries = Array.from(new Set(cities.map((city) => city.country)));
+
   return (
-    <div>
-      <h2>Country List</h2>
-    </div>
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.id} country={city} />
+      ))}
+    </ul>
   );
 }
