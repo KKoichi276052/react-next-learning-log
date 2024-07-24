1bff0e7a7a84879573c0b45222450da08a395b42
Koichi Kataoka <k.koichi276052@gmail.com>
2024-07-24 10:11:40 +0900

    clean up

--- ultimate-react-course/ultimate-react-course-main/13-workout-timer/starter/src/Calculator.js
+++ ultimate-react-course/ultimate-react-course-main/13-workout-timer/starter/src/Calculator.js
@@ -7,7 +7,9 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -33,9 +35,9 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>How many sets?</label>
           <input
-            type='range'
-            min='1'
-            max='5'
+            type="range"
+            min="1"
+            max="5"
             value={sets}
             onChange={(e) => setSets(e.target.value)}
           />
@@ -44,10 +46,10 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>How fast are you?</label>
           <input
-            type='range'
-            min='30'
-            max='180'
-            step='30'
+            type="range"
+            min="30"
+            max="180"
+            step="30"
             value={speed}
             onChange={(e) => setSpeed(e.target.value)}
           />
@@ -56,9 +58,9 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>Break length</label>
           <input
-            type='range'
-            min='1'
-            max='10'
+            type="range"
+            min="1"
+            max="10"
             value={durationBreak}
             onChange={(e) => setDurationBreak(e.target.value)}
           />
