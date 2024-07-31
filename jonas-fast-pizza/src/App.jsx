8e3025eca8c4d7218c281b5552119885e2dcbc2d
Koichi Kataoka <k.koichi276052@gmail.com>
2024-07-31 14:23:34 +0900

    jonas fast pizza define routing

--- jonas-fast-pizza/src/App.jsx
+++ jonas-fast-pizza/src/App.jsx
@@ -1,5 +1,40 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+import Cart from './features/cart/Cart';
+import Error from './ui/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '*',
+    element: <Error />,
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
