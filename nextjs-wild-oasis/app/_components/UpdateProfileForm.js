f74614777a31fa1b6d8999120afb7fe9f3517a8d
Koichi Kataoka <k.koichi276052@gmail.com>
2024-09-25 17:24:02 +0900

    jonas nextjs updateprofile wip

--- nextjs-wild-oasis/app/_components/UpdateProfileForm.js
+++ nextjs-wild-oasis/app/_components/UpdateProfileForm.js
@@ -2,18 +2,23 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
+import { updateGuest } from '../_lib/actions';
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ guest, children }) {
   const [country, setCountry] = useState('');
-  // CHANGE
-  const countryFlag = '/pt.jpg';
+  const { fullName, email, nationality, countryFlag, nationalID } = guest;
 
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -21,6 +26,8 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label>Email address</label>
         <input
+          defaultValue={email}
+          name="email"
           disabled
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -29,12 +36,12 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between relative">
           <label htmlFor="nationality">Where are you from?</label>
-          <Image
+          {/* <Image
             src={countryFlag}
             alt="Country flag"
             className="h-5 rounded-sm"
             fill
-          />
+          /> */}
         </div>
       </div>
 
@@ -43,6 +50,7 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
--- nextjs-wild-oasis/app/_lib/actions.js
+++ nextjs-wild-oasis/app/_lib/actions.js
@@ -1,6 +1,26 @@
 'use server';
 
-import { signIn, signOut } from './auth';
+import { auth, signIn, signOut } from './auth';
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error('You must be signed in to update your profile');
+
+  const nationalID = formData.get('nationalID');
+  const [nationality, countryFlag] = formData.get('nationality').split('%');
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error('Invalid national ID');
+  }
+
+  const updateData = {
+    nationalID,
+    nationality,
+    countryFlag,
+  };
+
+  console.log(updateData);
+}
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
