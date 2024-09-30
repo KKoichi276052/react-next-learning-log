0053ca78d615a9c9219a1859cfe3a24afa058a87
Koichi Kataoka <k.koichi276052@gmail.com>
2024-09-30 14:49:52 +0900

    jonas wild oasis wip

---
+++ wild-oasis/src/ui/ProtectedRoute.jsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { useUser } from '../features/authentication/useUser';
+import Spinner from './Spinner';
+import { useEffect } from 'react';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated, isLoading } = useUser();
+  const navigate = useNavigate();
+  console.log(isAuthenticated);
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate('/login');
+  }, [isAuthenticated, navigate, isLoading]);
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+}
+export default ProtectedRoute;
