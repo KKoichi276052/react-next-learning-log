e8808105d5ffd47cd520eda53aa4e5f97de342e7
Koichi Kataoka <k.koichi276052@gmail.com>
2024-12-16 11:48:59 +0900

    grider finish project wip

--- next-blog-v2/src/components/header/SearchInput.tsx
+++ next-blog-v2/src/components/header/SearchInput.tsx
@@ -1,7 +1,18 @@
-import { Input } from '@nextui-org/input';
+"use client";
+
+import { Input } from "@nextui-org/input";
+import { useSearchParams } from "next/navigation";
+import * as actions from "@/actions";
 
 const SearchInput = () => {
-  return <Input />;
+	const searchParams = useSearchParams();
+
+	return (
+		<form action={actions.search}>
+			<Input name="term" defaultValue={searchParams.get("term") || ""} />
+			<button type="submit" hidden />
+		</form>
+	);
 };
 
 export default SearchInput;
--- next-blog-v2/src/db/queries/posts.ts
+++ next-blog-v2/src/db/queries/posts.ts
@@ -1,12 +1,12 @@
-import type { Post } from '@prisma/client';
-import { db } from '@/lib/db';
+import type { Post } from "@prisma/client";
+import { db } from "@/lib/db";
 
 export type PostWithData = Post & {
-  topic: {
-    slug: string;
-  };
-  user: { name: string | null };
-  _count: { comments: number };
+	topic: {
+		slug: string;
+	};
+	user: { name: string | null };
+	_count: { comments: number };
 };
 
 // PostWithData represents a single post with additional data such as topic slug, user name, and comment count.
@@ -15,61 +15,87 @@ export type PostWithData = Post & {
 //   ReturnType<typeof fetchPostsByTopicSlug>
 // >[number];
 
+export function fetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+	return db.post.findMany({
+		include: {
+			topic: {
+				select: {
+					slug: true,
+				},
+			},
+			user: {
+				select: {
+					name: true,
+					image: true,
+				},
+			},
+			_count: {
+				select: {
+					comments: true,
+				},
+			},
+		},
+		where: {
+			OR: [{ title: { contains: term } }, { content: { contains: term } }],
+		},
+	});
+}
+
 export const fetchPostsByTopicSlug = (
-  slug: string
+	slug: string,
 ): Promise<PostWithData[]> => {
-  return db.post.findMany({
-    where: {
-      topic: {
-        slug,
-      },
-    },
-    include: {
-      topic: {
-        select: {
-          slug: true,
-        },
-      },
-      user: {
-        select: {
-          name: true,
-        },
-      },
-      _count: {
-        select: {
-          comments: true,
-        },
-      },
-    },
-  });
+	return db.post.findMany({
+		where: {
+			topic: {
+				slug,
+			},
+		},
+		include: {
+			topic: {
+				select: {
+					slug: true,
+				},
+			},
+			user: {
+				select: {
+					name: true,
+				},
+			},
+			_count: {
+				select: {
+					comments: true,
+				},
+			},
+		},
+	});
 };
 
 export const fetchTopPosts = async (): Promise<PostWithData[]> => {
-  return db.post.findMany({
-    orderBy: [
-      {
-        comments: {
-          _count: 'desc',
-        },
-      },
-    ],
-    include: {
-      topic: {
-        select: {
-          slug: true,
-        },
-      },
-      user: {
-        select: {
-          name: true,
-          image: true,
-        },
-      },
-      _count: {
-        select: {
-          comments: true,
-        },
-      },
-    },
-  });
+	return db.post.findMany({
+		orderBy: [
+			{
+				comments: {
+					_count: "desc",
+				},
+			},
+		],
+		include: {
+			topic: {
+				select: {
+					slug: true,
+				},
+			},
+			user: {
+				select: {
+					name: true,
+					image: true,
+				},
+			},
+			_count: {
+				select: {
+					comments: true,
+				},
+			},
+		},
+	});
 };
