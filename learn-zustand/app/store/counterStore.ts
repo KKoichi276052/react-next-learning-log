804497c31596ec93f19c4abf5ad164ccfd09d300
Koichi Kataoka <k.koichi276052@gmail.com>
2025-03-05 12:11:40 +0900

    add persist to save store to local storage

--- learn-zustand/app/store/counterStore.ts
+++ learn-zustand/app/store/counterStore.ts
@@ -1,21 +1,31 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 type CounterState = {
 	count: number;
-	interval: number;
+	// interval: number;
 	increment: () => void;
 	decrement: () => void;
 	reset: () => void;
 };
 
-export const useCounterStore = create<CounterState>((set) => ({
-	count: 0,
-	interval: 1,
-	increment: () => set((state) => ({ count: state.count + 1 })),
-	decrement: () =>
-		set((state) => ({ count: state.count > 0 ? state.count - 1 : 0 })),
-	reset: () => set({ count: 0 }),
-}));
+export const useCounterStore = create<CounterState>()(
+	devtools(
+		persist(
+			(set) => ({
+				count: 0,
+				// interval: 1,
+				increment: () => set((state) => ({ count: state.count + 1 })),
+				decrement: () =>
+					set((state) => ({ count: state.count > 0 ? state.count - 1 : 0 })),
+				reset: () => set({ count: 0 }),
+			}),
+			{
+				name: "counter",
+			},
+		),
+	),
+);
 
 export default useCounterStore;
 
